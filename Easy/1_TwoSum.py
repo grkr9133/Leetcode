e6c3915534a0a541fd875406c337d2c75d650ab4
grkr9133 <137760755+grkr9133@users.noreply.github.com>
2024-12-01 13:29:31 -0600

    Complete code for TWO_SUM Problem

--- Easy/1_TwoSum.py
+++ Easy/1_TwoSum.py
@@ -1,2 +1,10 @@
 #Leetcode Problem 1 TwoSum
 #Author: Radhakrishna Rakesh Grandi
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map={} #dictionary to store index:value pairs
+        for index,number in enumerate(nums):
+            if target-number in hash_map:   #finds if diff between target and current number in dictionary
+                return (hash_map[target-number],index)     #return index of the difference and the index of current number
+            hash_map[number]=index      #else appends the new number to the dictionary with key as index
+                
