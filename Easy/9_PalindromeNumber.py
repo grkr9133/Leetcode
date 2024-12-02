12912901825bbcfcba12eb04706a9582e344ac89
grkr9133 <137760755+grkr9133@users.noreply.github.com>
2024-12-02 09:51:38 -0600

    complete code for 9_palindromeNumber

--- Easy/9_PalindromeNumber.py
+++ Easy/9_PalindromeNumber.py
@@ -1,3 +1,22 @@
 #Leetcode Problem 9 Palindrome Number
 #Author: Radhakrishna Rakesh Grandi
 # Updated in December 01, 2024
+
+# best code 
+
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         return f"{x}" == f"{x}"[::-1]
+
+# My code
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        else:
+            string = str(x)
+            if string == string[::-1]:
+                return True
+            else:
+                return False
